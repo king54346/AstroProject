2eeb0705602af0ae85e2e947d9bdfad10eb46bb8
king54346 <35963228+king54346@users.noreply.github.com>
2023-07-09 15:59:45 +0800

    Add files via upload

---
+++ src/components/VueComponents/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-button>SSSS</el-button>
+</template>
+
+<script>
+import { ElButton } from 'element-plus'
+export default {
+  components: {
+    ElButton
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
